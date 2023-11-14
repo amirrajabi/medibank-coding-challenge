21d4d1854d5e2576ff9df5235e2077945cd00744
amirrajabi <amir.divergent@gmail.com>
2023-11-14 16:36:03 +1100

    fetch data in list

--- app/components/CatsList.tsx
+++ app/components/CatsList.tsx
@@ -1,6 +1,40 @@
-export default function CatsList() {
-  return (
-    <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+'use client'
 
+import React, { useState, useEffect } from 'react';
+
+interface DataItem {
+  name: string;
+  gender: string;
+  age: string;
+}
+
+const CatsList: React.FC = () => {
+  const [data, setData] = useState<DataItem[]>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch('https://gist.githubusercontent.com/medibank-digital/a1fc81a93200a7b9d5f8b7eae0fac6f8/raw/de10a4fcf717e6c431e88c965072c784808fd6b2/people.json');
+        const json: DataItem[] = await response.json();
+        setData(json);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {data.map((item) => (
+        <div key={item.name}>
+          <p>{item.gender}</p>
+          <p>{item.age}</p>
+        </div>
+      ))}
     </div>
-  )};
+  );
+};
+
+export default CatsList;
