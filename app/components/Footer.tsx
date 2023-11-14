50690c92a47fb88250a32c2ccf68648a62c29513
amirrajabi <amir.divergent@gmail.com>
2023-11-14 16:16:36 +1100

    First Push - setup UI

---
+++ app/components/Footer.tsx
@@ -0,0 +1,24 @@
+export default function Footer() {
+  return (
+    <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <a
+          href="https://github.com/amirrajabi/medibank-coding-challenge"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Amir Rajabi{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Medibank Coding Challenge
+          </p>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            11/2023
+          </p>
+        </a>
+      </div>
+  )};
---
+++ app/components/Header.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+
+export default function Header() {
+  return (
+    <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+            <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            <Image 
+                src="/catslist.jpg"
+                alt="Cats List Logo"
+                width={200}
+                height={48}
+                priority
+            />
+            </a>
+        </div>
+    </div>
+  )};
